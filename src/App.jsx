fbf098b39f74ee6db3f5c20b243c36dfe2d44ba5
Mirza Shahriar tonmoy <Shahriar.Tonmoy@contessabd.com>
2024-08-20 15:10:34 +0600

    Winning condition done

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,17 @@ function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   console.log(squares);
 
+  const theWinner = winner(squares);
+  let gameStatus;
+  if(theWinner){
+    gameStatus = 'The winner is ' + theWinner;
+  }else{
+    gameStatus = 'Move for ' + (isNext ? 'X' : 'O');
+  }
+
   function handleClick(i) {
     // if square is not null it will return so that that square value won't be overwritten
-    if (squares[i]) {
+    if (squares[i] || winner(squares)) {
       return;
     }
     // console.log(i);
@@ -31,6 +39,7 @@ function App() {
 
   return (
     <>
+      <div className='text-4xl text-green-600 text-center'>{gameStatus}</div>
       <div className='w-max mx-auto grid grid-cols-3 border'>
         {
           squares.map((number,index) => (
@@ -40,6 +49,26 @@ function App() {
       </div>
     </>
   )
+
+   function winner(squares) {
+    const conditions = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ];
+    for(let i=0; i<conditions.length; i++){
+      const [first, second, third] = conditions[i];
+      if(squares[first] && squares[first] === squares[second] && squares[first] === squares[third]){
+        return squares[first];
+      }
+    }
+    return null;
+  }
 }
 
 export default App
